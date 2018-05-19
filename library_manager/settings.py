aeabfc64f26f136bd07a6f0aa6153e089ce62660
V-Kopio <ville.kopio@gmail.com>
2018-05-20 00:53:29 +0300

    Added security settings for production environment

--- library_manager/settings.py
+++ library_manager/settings.py
@@ -23,6 +23,13 @@ if 'DJANGO_PRODUCTION' in os.environ:
         }
     }
 
+    SECURE_CONTENT_TYPE_NOSNIFF = True
+    SECURE_BROWSER_XSS_FILTER = True
+    SECURE_SSL_REDIRECT = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+    X_FRAME_OPTIONS = 'DENY'
+
 else:
     # DEVELOPMENT SETTINGS
     # ====================
