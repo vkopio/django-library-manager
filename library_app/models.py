af729b187becf9dbf091c45c8c9237543609f908
V-Kopio <ville.kopio@gmail.com>
2018-05-20 23:23:33 +0300

    Added models for Book, Author and Genre

--- library_app/models.py
+++ library_app/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    name = models.CharField(max_length=255)
+    isbn = models.CharField(max_length=255)
+    description = models.TextField()
+    pub_date = models.DateField()
+
+    def __str__(self):
+        return self.name
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    birth_date = models.DateField()
+    books = models.ManyToManyField(Book)
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=255)
+    books = models.ManyToManyField(Book)
+
+    def __str__(self):
+        return self.name
