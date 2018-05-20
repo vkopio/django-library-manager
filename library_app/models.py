438e9a842ad7d2902cbdb15fbdc279627b4f3a1c
V-Kopio <ville.kopio@gmail.com>
2018-05-20 23:29:45 +0300

    Changed books to have genres and authors instead of vice versa

--- library_app/models.py
+++ library_app/models.py
@@ -1,21 +1,10 @@
 from django.db import models
 
 
-class Book(models.Model):
-    name = models.CharField(max_length=255)
-    isbn = models.CharField(max_length=255)
-    description = models.TextField()
-    pub_date = models.DateField()
-
-    def __str__(self):
-        return self.name
-
-
 class Author(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     birth_date = models.DateField()
-    books = models.ManyToManyField(Book)
 
     def __str__(self):
         return self.first_name + ' ' + self.last_name
@@ -23,7 +12,18 @@ class Author(models.Model):
 
 class Genre(models.Model):
     name = models.CharField(max_length=255)
-    books = models.ManyToManyField(Book)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=255)
+    isbn = models.CharField(max_length=255)
+    description = models.TextField()
+    pub_date = models.DateField()
+    authors = models.ManyToManyField(Author)
+    genres = models.ManyToManyField(Genre)
 
     def __str__(self):
         return self.name
