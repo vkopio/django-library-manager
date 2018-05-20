93f8a103c8804f9887d2f4646cca615dd9cec54b
V-Kopio <ville.kopio@gmail.com>
2018-05-20 23:23:06 +0300

    Added library_app to INSTALLED_APPS

--- library_site/settings.py
+++ library_site/settings.py
@@ -48,6 +48,7 @@ ALLOWED_HOSTS = ['localhost']
 # Application definition
 
 INSTALLED_APPS = [
+    'library_app.apps.LibraryAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
