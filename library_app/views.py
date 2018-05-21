e00bb53c8124c8ebcc5c62a90e1bb66c31330b94
V-Kopio <ville.kopio@gmail.com>
2018-05-21 19:36:55 +0300

    Added index view which shows the count of book, author and genre instances

--- library_app/views.py
+++ library_app/views.py
@@ -1,5 +1,16 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Book, Author, Genre
 
 
 def index(request):
-    return HttpResponse("Hello, world!")
+    book_count = Book.objects.all().count
+    author_count = Author.objects.all().count
+    genre_count = Genre.objects.all().count
+
+    context = {
+        'book_count': book_count,
+        'author_count': author_count,
+        'genre_count': genre_count
+    }
+
+    return render(request, 'index.html', context)
