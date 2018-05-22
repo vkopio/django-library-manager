8110a4d22b42a86dc28b8230cf994ba1fcdd4a42
V-Kopio <ville.kopio@gmail.com>
2018-05-23 02:21:49 +0300

    Added redirection from url path root to library app

--- library_site/urls.py
+++ library_site/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
+
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='library', permanent=False)),
     path('library/', include('library_app.urls')),
     path('admin/', admin.site.urls),
 ]
