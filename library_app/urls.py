d10f227dbc6256ea7d85e314326db9661b3f085e
V-Kopio <ville.kopio@gmail.com>
2018-05-23 02:15:43 +0300

    Simplified book list view name

--- library_app/urls.py
+++ library_app/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('books/', views.BookListView.as_view(), name='book_list_view'),
+    path('books/', views.BookListView.as_view(), name='book_list'),
     path('books/<int:pk>/', views.BookDetailView.as_view(), name='book_detail'),
 ]
