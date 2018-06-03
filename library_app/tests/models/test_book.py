128778f533c51431d757f4bc508d63eb4e137f5f
V-Kopio <ville.kopio@gmail.com>
2018-06-03 21:29:49 +0300

    Added tests for isbn validation

--- library_app/tests/models/test_book.py
+++ library_app/tests/models/test_book.py
@@ -1,11 +1,16 @@
-from django.test import TestCase
+from library_app.tests.utils.extended_test_case import ExtendedTestCase
 from library_app.tests.utils.factories import create_book
-from library_app.models import Book
 
 
-class BookTestCase(TestCase):
+class BookTestCase(ExtendedTestCase):
     def setUp(self):
         self.book = create_book("test-book", "")
 
     def test_str_returns_name(self):
         self.assertEqual(self.book.__str__(), "test-book")
+
+    def test_raises_error_for_invalid_isbn(self):
+        self.book.isbn = 'invalid-isbn'
+
+        with self.assertValidationError('isbn', ['invalid-isbn is not a valid ISBN number']):
+            self.book.full_clean()
