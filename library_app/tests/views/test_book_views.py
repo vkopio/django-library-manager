e6bb8abf783950c16494247e0a2520d390a1f8ac
V-Kopio <ville.kopio@gmail.com>
2018-06-03 20:16:08 +0300

    Added book view tests

---
+++ library_app/tests/views/test_book_views.py
@@ -0,0 +1,35 @@
+from django.test import TestCase
+from django.urls import reverse
+from library_app.tests.utils.factories import create_book
+
+
+class BookViewTests(TestCase):
+    def test_list_with_no_books(self):
+        self.__get_list_response()
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_list_with_a_book(self):
+        book = create_book('test', 'test')
+
+        self.__get_list_response()
+        self.assertIn(book, self.response.context['book_list'])
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_detail_with_valid_id(self):
+        book = create_book('test', 'test')
+
+        self.__get_detail_response(book.id)
+        self.assertEqual(self.response.context['book'], book)
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_detail_with_invalid_id(self):
+        self.__get_detail_response(1)
+        self.assertEqual(self.response.status_code, 404)
+
+    def __get_list_response(self):
+        url = reverse('library_app:book_list')
+        self.response = self.client.get(url)
+
+    def __get_detail_response(self, book_id):
+        url = reverse('library_app:book_detail', args=(book_id,))
+        self.response = self.client.get(url)
