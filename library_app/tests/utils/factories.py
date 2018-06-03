f36e15985b5b8b793bd6f79231ce578342d21a50
V-Kopio <ville.kopio@gmail.com>
2018-06-03 16:14:25 +0300

    Added test utils which make it easier to create objects

---
+++ library_app/tests/utils/factories.py
@@ -0,0 +1,29 @@
+from django.utils import timezone
+from library_app.models import Book, Author, Genre
+
+
+def __time():
+    return timezone.now()
+
+
+def create_book(name='', isbn='', description=''):
+    return Book.objects.create(
+        name=name,
+        isbn=isbn,
+        description=description,
+        pub_date=__time()
+    )
+
+
+def create_author(first_name='', last_name=''):
+    return Author.objects.create(
+        first_name=first_name,
+        last_name=last_name,
+        birth_date=__time()
+    )
+
+
+def create_genre(name=''):
+    return Genre.objects.create(
+        name=name,
+    )
