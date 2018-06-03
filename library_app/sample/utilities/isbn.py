68e0a074e89dd891f8f481aac84c5d03ca23ad05
V-Kopio <ville.kopio@gmail.com>
2018-06-03 21:51:52 +0300

    Changed isbn util to use stdnum library so validations contain also ISBN 13

--- library_app/sample/utilities/isbn.py
+++ library_app/sample/utilities/isbn.py
@@ -1,16 +1,11 @@
-import re
+from stdnum import isbn
+from stdnum.exceptions import InvalidFormat
 
 
-def valid_isbn(isbn):
-    isbn = isbn.replace('-', '').replace(' ', '').upper()
-    match = re.search(r'^(\d{9})(\d|X)$', isbn)
+def valid_isbn(value):
+    try:
+        isbn.validate(value)
+        return True
 
-    if not match:
+    except InvalidFormat:
         return False
-
-    digits = match.group(1)
-    check_digit = 10 if match.group(2) == 'X' else int(match.group(2))
-
-    result = sum((i + 1) * int(digit) for i, digit in enumerate(digits))
-
-    return (result % 11) == check_digit
