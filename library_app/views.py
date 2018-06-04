27e6796a12fb222151c7a276bfa13b1199dd1062
V-Kopio <ville.kopio@gmail.com>
2018-06-05 01:43:52 +0300

    Added initial view for creating reservations

--- library_app/views.py
+++ library_app/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import generic
+from django.contrib.auth.decorators import login_required
 
 from .apps import app_dir
-from .models import Book, Author, Genre
+from .models import Book, Author, Genre, Reservation
 
 
 def index(request):
@@ -24,3 +25,15 @@ class BookListView(generic.ListView):
 
 class BookDetailView(generic.DetailView):
     model = Book
+
+
+@login_required
+def book_reservation(request, book_id):
+    book = Book.objects.get(pk=book_id)
+
+    if True:
+        reservation = Reservation(book=book, reserver=request.user)
+        reservation.full_clean()
+        reservation.save()
+
+    return redirect(book)
