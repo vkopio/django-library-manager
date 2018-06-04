423a930cc123f249883fb54262a9263039cb0398
V-Kopio <ville.kopio@gmail.com>
2018-06-04 12:43:25 +0300

    Added reservations and borrowings to admin panel

--- library_app/admin.py
+++ library_app/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-
-
 from .models import *
 
 admin.site.register(Book)
 admin.site.register(Author)
 admin.site.register(Genre)
+admin.site.register(Reservation)
+admin.site.register(Borrowing)
