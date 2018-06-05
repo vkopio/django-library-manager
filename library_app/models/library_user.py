9362fa3b56113dabcee0101e1f5ce47104c2f8da
V-Kopio <ville.kopio@gmail.com>
2018-06-05 15:03:18 +0300

    Added method to library_user which returns the reservations of the user

--- library_app/models/library_user.py
+++ library_app/models/library_user.py
@@ -4,3 +4,7 @@ from django.contrib.auth.models import User
 class LibraryUser(User):
     class Meta:
         proxy = True
+
+    def reservations(self):
+        from .reservation import Reservation
+        return Reservation.objects.filter(reserver=self)
