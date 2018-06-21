c3d4b40331738487bb329de54f3040fb645489c5
V-Kopio <ville.kopio@gmail.com>
2018-06-21 18:15:13 +0300

    Added a validation for reservations so that user cannot reserve the same book multiple times

--- library_app/models/reservation.py
+++ library_app/models/reservation.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
 from library_app.models import Book, LibraryUser
 from ._base import BaseModel
 
@@ -16,6 +18,12 @@ class Reservation(BaseModel):
         on_delete=models.CASCADE,
         unique=False
     )
+
+    def clean(self):
+        reserved_the_same_book = Reservation.objects.filter(reserver=self.reserver, book=self.book)
+
+        if reserved_the_same_book:
+            raise ValidationError(_('User has already reserved this book.'))
 
     def __str__(self):
         return self.book.name + ', reserver: ' + self.reserver.username
