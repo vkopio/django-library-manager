5f3866e3851da3f3cf95e25b5ff00a50f7ea2dcc
V-Kopio <ville.kopio@gmail.com>
2018-07-17 19:10:54 +0300

    Added reservation_queue_position test for book views

--- library_app/tests/views/test_book_views.py
+++ library_app/tests/views/test_book_views.py
@@ -1,9 +1,9 @@
-from django.test import TestCase
 from django.urls import reverse
+from library_app.tests.extended_test_case import ExtendedTestCase
 from library_app.sample.utilities.factories import create_book
 
 
-class BookViewTests(TestCase):
+class BookViewTests(ExtendedTestCase):
     def test_list_with_no_books(self):
         self.__get_list_response()
         self.assertEqual(self.response.status_code, 200)
@@ -25,6 +25,15 @@ class BookViewTests(TestCase):
     def test_detail_with_invalid_id(self):
         self.__get_detail_response(1)
         self.assertEqual(self.response.status_code, 404)
+
+    def test_detail_with_logged_in_user(self):
+        book = create_book('test', 'test')
+
+        self.login()
+        self.__get_detail_response(book.id)
+        self.assertEqual(self.response.context['book'], book)
+        self.assertEqual(self.response.context['reservation_queue_position'], 0)
+        self.assertEqual(self.response.status_code, 200)
 
     def __get_list_response(self):
         url = reverse('library_app:book_list')
