1e842da1d86dc6b32a1f7ad9238e20ae7df1fffe
V-Kopio <ville.kopio@gmail.com>
2018-07-17 23:15:59 +0300

    Added a test for book borrowings_count update

--- library_app/tests/models/test_borrowing.py
+++ library_app/tests/models/test_borrowing.py
@@ -7,5 +7,8 @@ class GenreTestCase(ExtendedTestCase):
         self.book = create_book("test-book", "")
         self.borrowing = create_borrowing('1970-01-01', book=self.book, borrower=self.user.libraryuser)
 
+    def test_book_borrowings_count_is_updated(self):
+        self.assertEqual(self.book.borrowings_count, 1)
+
     def test_str_returns_desired_format(self):
         self.assertEqual(self.borrowing.__str__(), "test-book, borrower: test_user, due date: 1970-01-01")
