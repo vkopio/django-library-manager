1b8d982c06c4790bbc52daa75458dadee3e18a8e
V-Kopio <ville.kopio@gmail.com>
2018-07-17 19:26:59 +0300

    Added borrowing tests

---
+++ library_app/tests/models/test_borrowing.py
@@ -0,0 +1,11 @@
+from library_app.tests.extended_test_case import ExtendedTestCase
+from library_app.sample.utilities.factories import create_book, create_borrowing
+
+
+class GenreTestCase(ExtendedTestCase):
+    def setUp(self):
+        self.book = create_book("test-book", "")
+        self.borrowing = create_borrowing('1970-01-01', book=self.book, borrower=self.user.libraryuser)
+
+    def test_str_returns_desired_format(self):
+        self.assertEqual(self.borrowing.__str__(), "test-book, borrower: test_user, due date: 1970-01-01")
