04c56f03bd120e3bb7d7cdc043f01a41161326bd
V-Kopio <ville.kopio@gmail.com>
2018-07-18 01:15:42 +0300

    Added most_borrowed to book manager

--- library_app/models/managers/book_manager.py
+++ library_app/models/managers/book_manager.py
@@ -4,3 +4,6 @@ from django.db import models
 class BookManager(models.Manager):
     def newest(self, limit=1):
         return self.order_by('-creation_date')[:limit]
+
+    def most_borrowed(self, limit=1):
+        return self.order_by('-borrowings_count')[:limit]
