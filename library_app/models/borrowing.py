a91720f1c6163a2167c9edb24c6dc3227f07e538
V-Kopio <ville.kopio@gmail.com>
2018-07-17 23:15:30 +0300

    Added signal to borrowing creation which updates book borrowings_count. This is done to preserve info about borrowings after the objects itself are deleted.

--- library_app/models/borrowing.py
+++ library_app/models/borrowing.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from library_app.models import Book, LibraryUser
 from ._base import BaseModel
 
@@ -11,3 +13,10 @@ class Borrowing(BaseModel):
 
     def __str__(self):
         return self.book.name + ', borrower: ' + self.borrower.user.username + ', due date: ' + str(self.due_date)
+
+
+@receiver(post_save, sender=Borrowing)
+def update_book_borrowings_count(sender, instance, **kwargs):
+    book = instance.book
+    book.borrowings_count += 1
+    book.save()
