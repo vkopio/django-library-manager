67090589253c05ffe6d827a6f614eefdd7093d7b
V-Kopio <ville.kopio@gmail.com>
2018-07-18 01:31:51 +0300

    Included newest and most_borrowed books in index tests

--- library_app/tests/views/test_index_view.py
+++ library_app/tests/views/test_index_view.py
@@ -6,21 +6,23 @@ from library_app.sample.utilities.factories import create_book, create_author, c
 class IndexViewTests(TestCase):
     def test_with_no_objects(self):
         self.__get_response()
-        self.__assert_context(0, 0, 0)
+        self.__assert_context(0, 0, 0, [], [])
 
     def test_with_objects(self):
-        create_book('test', 'test')
+        book = create_book('test', 'test')
         create_author('test', 'test')
         create_genre('test')
 
         self.__get_response()
-        self.__assert_context(1, 1, 1)
+        self.__assert_context(1, 1, 1, [book], [book])
 
     def __get_response(self):
         self.response = self.client.get(reverse('library_app:index'))
 
-    def __assert_context(self, book_count, author_count, genre_count):
+    def __assert_context(self, book_count, author_count, genre_count, newest_books, most_borrowed_books):
         self.assertEqual(self.response.status_code, 200)
         self.assertEqual(self.response.context['book_count'], book_count)
         self.assertEqual(self.response.context['author_count'], author_count)
         self.assertEqual(self.response.context['genre_count'], genre_count)
+        self.assertEqual(list(self.response.context['newest_books']), newest_books)
+        self.assertEqual(list(self.response.context['most_borrowed_books']), most_borrowed_books)
