6489214a271fecf32fae309f72c45fa5c4d8b9af
V-Kopio <ville.kopio@gmail.com>
2018-07-18 01:22:10 +0300

    Added 5 newest and most borrowed books to the front page

--- library_app/views/index_view.py
+++ library_app/views/index_view.py
@@ -6,7 +6,9 @@ def index(request):
     context = {
         'book_count': Book.objects.all().count(),
         'author_count': Author.objects.all().count(),
-        'genre_count': Genre.objects.all().count()
+        'genre_count': Genre.objects.all().count(),
+        'newest_books': Book.objects.newest(5),
+        'most_borrowed_books': Book.objects.most_borrowed(5)
     }
 
     return render(request, 'library_app/index.html', context)
