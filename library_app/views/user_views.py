0b15f6768e646b370300b9db3f2cbe3e7a600475
V-Kopio <ville.kopio@gmail.com>
2018-07-17 20:07:12 +0300

    Fixed user_views function calls

--- library_app/views/user_views.py
+++ library_app/views/user_views.py
@@ -6,8 +6,8 @@ from django.contrib.auth.decorators import login_required
 def user_detail(request):
     user = request.user.libraryuser
     context = {
-        'reservations': user.reservations,
-        'borrowings': user.borrowings
+        'reservations': user.reservations(),
+        'borrowings': user.borrowings()
     }
 
     return render(request, 'library_app/user_detail.html', context)
