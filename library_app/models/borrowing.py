34a1a0ad6eaf71bd72704e19cb76242929164f21
V-Kopio <ville.kopio@gmail.com>
2018-07-19 01:18:29 +0300

    Changed borrowing borrower and lender to be one-to-many fields instead of one-to-one so one person can lend or borrow more than one book...

--- library_app/models/borrowing.py
+++ library_app/models/borrowing.py
@@ -8,8 +8,8 @@ from ._base import BaseModel
 class Borrowing(BaseModel):
     due_date = models.DateField()
     book = models.OneToOneField(Book, on_delete=models.CASCADE)
-    borrower = models.OneToOneField(LibraryUser, related_name='borrower', on_delete=models.SET_NULL, null=True)
-    lender = models.OneToOneField(LibraryUser, related_name='lender', on_delete=models.SET_NULL, null=True)
+    borrower = models.ForeignKey(LibraryUser, related_name='borrower', on_delete=models.SET_NULL, null=True)
+    lender = models.ForeignKey(LibraryUser, related_name='lender', on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.book.name + ', borrower: ' + self.borrower.user.username + ', due date: ' + str(self.due_date)
