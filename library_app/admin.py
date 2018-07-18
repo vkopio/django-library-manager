f3bbe43159f1685e4badfa827f752a1f4653c2d6
V-Kopio <ville.kopio@gmail.com>
2018-07-19 00:45:49 +0300

    Changed admin list views to show extra info

--- library_app/admin.py
+++ library_app/admin.py
@@ -1,8 +1,29 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Book)
-admin.site.register(Author)
-admin.site.register(Genre)
-admin.site.register(Reservation)
-admin.site.register(Borrowing)
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'creation_date', 'modification_date')
+
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('name', 'pub_date', 'creation_date', 'modification_date')
+
+
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('name', 'creation_date', 'modification_date')
+
+
+class BorrowingAdmin(admin.ModelAdmin):
+    list_display = ('book', 'borrower', 'lender', 'due_date', 'creation_date')
+
+
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = ('book', 'reserver', 'creation_date')
+
+
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Book, BookAdmin)
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(Borrowing, BorrowingAdmin)
+admin.site.register(Reservation, ReservationAdmin)
